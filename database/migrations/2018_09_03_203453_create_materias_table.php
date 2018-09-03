8b125a9a25a64cc95eb38dbab02d6048d1172cf5
Israel Baltazar <andressolis1@hotmail.es>
2018-09-03 17:14:20 -0400

    tengo hambre perro

---
+++ database/migrations/2018_09_03_203453_create_materias_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMateriasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('materias', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('crn',10);
+            $table->string('calendario',15);
+            $table->string('materia');
+            $table->time('hora_inicio');
+            $table->string('salon');
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('materias');
+    }
+}
